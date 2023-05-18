caee6eab789368d93f71c3e0d5620c95debe2a57
freaklucas <lucas.oliveira.freak@gmail.com>
2023-05-18 13:00:28 -0300

    align basic css

--- src/pages/Imobi/index.jsx
+++ src/pages/Imobi/index.jsx
@@ -1,5 +1,15 @@
 import TopBanner from "../../components/TopBanner";
-import { Container, Left, ProfileAvatar, Right, Thumb, Profile, Description } from "./styles";
+import { Container, 
+  Left, 
+  ProfileAvatar, 
+  Right, 
+  Thumb, 
+  Profile, 
+  Description, 
+  ProfileDescription,
+  ProfileContact,
+  ProfileFormContact 
+} from "./styles";
 
 const Imobi = () => {
   return (
@@ -22,7 +32,25 @@ const Imobi = () => {
             <ProfileAvatar>
               <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="imagem avatar usuario" />
             </ProfileAvatar>
+            <ProfileDescription>
+              <h3>Lucas</h3>
+              <p>Descrição do usuario</p>
+            </ProfileDescription>
           </Profile>
+          <ProfileContact>
+          <h3>Informações para contato</h3>
+              <p>(64) 912348299</p>
+              <p>lucas@gmail.com</p>
+          </ProfileContact>
+          <ProfileFormContact>
+            <h3>Contrate o anunciate</h3>
+            <form>
+              <input type="text" />
+              <input type="text" />
+              <textarea type="text" />
+              <button>Enviar mensagem</button>
+            </form>
+          </ProfileFormContact>
         </Right>
       </Container>
     </>
--- src/pages/Imobi/styles.js
+++ src/pages/Imobi/styles.js
@@ -1,27 +1,59 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  padding: 20px 150px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const Left = styled.div`
-  
+  width: 70%;
+  padding: 10px;
+  border: 1px border solid rgb(0,0,0,.1);
 `;
 
 export const Right = styled.div`
-  
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 5px;
+
+  }
 `;
 
-export const Thumb = styled.div`
-  
-`;
 
 export const Description = styled.div`
-  
+  padding: 30px 0;
+  h2, p {
+    margin-bottom: 15px;
+  }
 `;
+
+  export const Thumb = styled.div`
+    img {
+      max-width: 100%;
+    }
+`;
+
 export const Profile = styled.div`
   
 `;
 export const ProfileAvatar = styled.div`
+  img {
+    max-width: 10%;
+  }
+`;
+
+export const ProfileDescription = styled.div`
+  
+`;
+
+export const ProfileContact = styled.div`
+  
+`;
+
+export const ProfileFormContact = styled.div`
   
 `;
